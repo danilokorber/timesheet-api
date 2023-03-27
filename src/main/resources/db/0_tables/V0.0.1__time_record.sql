49584e5a935e258987d45851c8d12570fd59fd3a
Danilo Körber <58609588+danilokorber@users.noreply.github.com>
2023-03-27 20:53:56 +0200

    Update V0.0.1__time_record.sql

--- src/main/resources/db/0_tables/V0.0.1__time_record.sql
+++ src/main/resources/db/0_tables/V0.0.1__time_record.sql
@@ -1,9 +1,7 @@
 CREATE TABLE time_record (
   id VARCHAR(36) NOT NULL UNIQUE,
   employee_id VARCHAR(36) NOT NULL,
-  start_time TIMESTAMP WITH TIME ZONE,
-  end_time TIMESTAMP WITH TIME ZONE,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP,
   PRIMARY KEY (id)
 );
-
-COMMIT;
